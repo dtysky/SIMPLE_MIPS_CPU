5a4c980e0ac3c54bcad0e7d12661c6508729ac9c
dtysky <dtysky@outlook.com>
2015-01-18 16:12:01 +0800

    Wait for test CONTROL_UNIT.
    Signed-off-by: dtysky <dtysky@outlook.com>

---
+++ CONTROL_UNIT/CONTROL_UNIT.srcs/sim_1/new/CONTROL_UNIT_TB.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Xilinx
+// Engineer: dtysky
+// 
+// Create Date: 2015/01/18 16:09:04
+// Design Name: CONTROL_UNIT
+// Module Name: CONTROL_UNIT_TB
+// Project Name: MISP_CPU
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CONTROL_UNIT_TB(
+
+    );
+endmodule
